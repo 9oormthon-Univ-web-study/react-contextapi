a76e1bdcad8fb8882a53c2aa8e71886654c7218c
kojesung <js95112345@gmail.com>
2024-09-08 21:30:18 +0900

    feat: ui추가

--- react-mobx-app/src/App.js
+++ react-mobx-app/src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 
 function App({ myCounter }) {
-    return <div className="App">{myCounter.count}</div>;
+    return (
+        <div className="App">
+            <div>{myCounter.count}</div>
+            <br />
+            <br />
+            <div>마이너스?? {myCounter.isNegative}</div>
+            <br />
+            <br />
+            <button onClick={() => myCounter.increase()}>+</button>
+            <button onClick={() => myCounter.decrease()}>-</button>
+        </div>
+    );
 }
 
 export default App;
