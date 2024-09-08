a2fd51017c453026503a8858f0e4e407318e7490
kojesung <js95112345@gmail.com>
2024-09-08 22:03:07 +0900

    feat : observer 호출을 통한 구독으로 액션 실행 시 감지하고 리렌더링 ui반영되도록 수정

--- react-mobx-app/src/App.js
+++ react-mobx-app/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
+import { observer } from 'mobx-react';
 
-function App({ myCounter }) {
+//observer를 통해 스토어 구독(or Provier 사용하는 방법도 있음)
+//구독하지 않으면 액션을 호출해서 observable한 값을 변화시켜도 반영이 되지 않음(값이 변화하긴 함)
+const App = observer(({ myCounter }) => {
     return (
         <div className="App">
             <div>{myCounter.count}</div>
@@ -13,6 +16,6 @@ function App({ myCounter }) {
             <button onClick={() => myCounter.decrease()}>-</button>
         </div>
     );
-}
+});
 
 export default App;
