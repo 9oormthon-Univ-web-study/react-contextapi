4a76d8f6e9e2088c024352f040e55b189e3bf0b2
kojesung <js95112345@gmail.com>
2024-09-14 23:43:37 +0900

    feat : textState atom을 위한 get을 활용한 ReadOnly selector 추가

--- react-recoil-app/src/App.tsx
+++ react-recoil-app/src/App.tsx
@@ -1,18 +1,33 @@
-import './App.css';
-import { atom } from 'recoil';
+import React from 'react';
+import { atom, selector, RecoilState, RecoilValueReadOnly } from 'recoil';
 import TextInput from './TextInput';
+import './App.css';
 
-export const textState = atom({
+// 텍스트 상태의 타입 정의
+type TextState = string;
+
+// atom의 타입 명시
+export const textState: RecoilState<TextState> = atom({
     key: 'textState',
     default: '',
 });
 
-function App() {
+// selector의 타입 명시
+export const charCounterState: RecoilValueReadOnly<number> = selector({
+    key: 'charCounterState',
+    get: ({ get }) => {
+        const text = get(textState);
+        return text.length;
+    },
+});
+
+// React 컴포넌트의 타입 명시
+const App: React.FC = () => {
     return (
         <div className="App">
             <TextInput />
         </div>
     );
-}
+};
 
 export default App;
--- react-recoil-app/src/TextInput.tsx
+++ react-recoil-app/src/TextInput.tsx
@@ -3,6 +3,7 @@ import { useRecoilState } from 'recoil';
 import { textState } from './App';
 
 const TextInput: React.FC = () => {
+    // useRecoilState()가 자동으로 구독해주기 때문에 atom은 다른 컴포넌트에 있지만 자동으로 최신화가 됨
     const [text, setText] = useRecoilState(textState);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
