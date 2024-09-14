06832adbf801b84380d3f728cb3353a2162837e8
kojesung <js95112345@gmail.com>
2024-09-15 00:01:22 +0900

    feat : useRecoilValue()로 읽기 전용 상태 렌더링 추가

--- react-recoil-app/src/App.tsx
+++ react-recoil-app/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { atom, selector, RecoilState, RecoilValueReadOnly } from 'recoil';
 import TextInput from './TextInput';
 import './App.css';
+import Character from './Character';
 
 // 텍스트 상태의 타입 정의
 type TextState = string;
@@ -16,16 +17,19 @@ export const textState: RecoilState<TextState> = atom({
 export const charCounterState: RecoilValueReadOnly<number> = selector({
     key: 'charCounterState',
     get: ({ get }) => {
+        // get함수로 반환한 새로운 값은 읽기 전용, 쓰려면 set사용해야함
         const text = get(textState);
         return text.length;
     },
 });
+// set말고 상태를 변경하는 `useSetRecoilState()`라는 함수도 존재하는데 이는 간단한 상황에서 변경, `set`은 복잡한 로직을 캡슐화
 
 // React 컴포넌트의 타입 명시
 const App: React.FC = () => {
     return (
         <div className="App">
             <TextInput />
+            <Character />
         </div>
     );
 };
