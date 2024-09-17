d3ac68dd126fb9b45de626b52d440adc9cedae04
kojesung <js95112345@gmail.com>
2024-09-17 19:36:36 +0900

    feat : 리스트 삭제 및 수정 기능

--- react-recoil-app/src/TodoItem.tsx
+++ react-recoil-app/src/TodoItem.tsx
@@ -12,7 +12,6 @@ interface TodoItemProps {
 }
 const TodoItem: React.FC<TodoItemProps> = ({ item }) => {
     const [todoLists, setTodoLists] = useRecoilState(todoList);
-    const [text, setText] = useState<string>('');
 
     // findIndex메서드로 전체 배열에서 props로 전달받은 item과 같은 값의 인덱스 반환
     const index = todoLists.findIndex((todoItem) => todoItem === item);
@@ -30,9 +29,30 @@ const TodoItem: React.FC<TodoItemProps> = ({ item }) => {
         setTodoLists(newList);
     };
 
+    //replacdItemAtIndex 활용해서 isCompleted 반전
+    const toggleItemCompletion = () => {
+        const newList = replaceItemAtIndex(todoLists, index, {
+            ...item,
+            isCompleted: !item.isCompleted,
+        });
+        setTodoLists(newList);
+    };
+
+    const removeItemAtIndex = (arr: TodoState, index: number) => {
+        return [...arr.slice(0, index), ...arr.slice(index + 1)];
+    };
+
+    const deleteItem = () => {
+        const newList = removeItemAtIndex(todoLists, index);
+        setTodoLists(newList);
+    };
+
     return (
         <div>
             <input type="text" value={item.text} onChange={editItemText} />
+            {/* onClick으로 해주면 default check 옵션을 부여해줘야함, 그래서 onChange로 구현 */}
+            <input type="checkbox" checked={item.isCompleted} onChange={toggleItemCompletion} />
+            <button onClick={deleteItem}>X</button>
         </div>
     );
 };
