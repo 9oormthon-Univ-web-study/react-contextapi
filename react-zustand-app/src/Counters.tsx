d98d82309487a0dca442a9388db6bd21c58d578c
kojesung <js95112345@gmail.com>
2024-09-18 16:25:57 +0900

    feat : zustand counter 기능 구현

---
+++ react-zustand-app/src/Counters.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useCounterStore } from './store';
+
+const Counters: React.FC = () => {
+    const { count, increment, reset, setNumber } = useCounterStore();
+
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newValue = event.target.value;
+        if (newValue === '' || isNaN(Number(newValue))) {
+            setNumber(0);
+        } else {
+            setNumber(Number(newValue));
+        }
+    };
+
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={increment}>Increment</button>
+            <button onClick={reset}>Reset</button>
+            <input type="number" value={count} onChange={handleInput} placeholder="Set number" />
+        </div>
+    );
+};
+
+export default Counters;
---
+++ react-zustand-app/src/store.tsx
@@ -0,0 +1,17 @@
+import { create } from 'zustand';
+
+// 스토어의 상태와 액션을 정의하는 인터페이스
+interface CounterStore {
+    count: number;
+    increment: () => void;
+    reset: () => void;
+    setNumber: (number: number) => void;
+}
+
+// zustand의 store는 hooks로 구성되어있으며 store를 생성할 때는 create 메서드를 활용함! set함수는 상태를 변경할 수 있음
+export const useCounterStore = create<CounterStore>((set) => ({
+    count: 1,
+    increment: () => set((state) => ({ count: state.count + 1 })),
+    reset: () => set({ count: 1 }), // count 1로 만드는 메서드 추가
+    setNumber: (number) => set({ count: number }),
+}));
