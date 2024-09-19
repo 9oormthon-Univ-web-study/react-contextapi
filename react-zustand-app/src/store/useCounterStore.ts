1e3d239a6b558db28e69a99ae81f07aa61ec2a20
kojesung <js95112345@gmail.com>
2024-09-19 19:27:43 +0900

    feat : storage 저장 기능 테스트

--- react-zustand-app/src/store/useCounterStore.ts
+++ react-zustand-app/src/store/useCounterStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 // 스토어의 상태와 액션을 정의하는 인터페이스
 interface CounterStore {
@@ -9,9 +10,17 @@ interface CounterStore {
 }
 
 // zustand의 store는 hooks로 구성되어있으며 store를 생성할 때는 create 메서드를 활용함! set함수는 상태를 변경할 수 있음
-export const useCounterStore = create<CounterStore>((set) => ({
-    count: 1,
-    increment: () => set((state) => ({ count: state.count + 1 })),
-    reset: () => set({ count: 1 }), // count 1로 만드는 메서드 추가
-    setNumber: (number) => set({ count: number }),
-}));
+export const useCounterStore = create<CounterStore>()(
+    //zustand V4부터는 create 사용법이 바뀌었다고 함..
+    persist(
+        (set) => ({
+            count: 1,
+            increment: () => set((state) => ({ count: state.count + 1 })),
+            reset: () => set({ count: 1 }), // count 1로 만드는 메서드 추가
+            setNumber: (number) => set({ count: number }),
+        }),
+        {
+            name: 'counter', // 로컬 스토리지에 저장될 키 이름
+        }
+    )
+);
--- react-zustand-app/src/store/useTodoStore.ts
+++ react-zustand-app/src/store/useTodoStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface Todo {
     text: string;
@@ -20,30 +21,35 @@ const getId = () => {
     return id++;
 };
 
-export const useTodoStore = create<TodoStore>((set) => ({
-    todos: [],
-    isCompleted: false,
-    addTodo: (todoText: string) =>
-        set((state) => ({
-            todos: [
-                ...state.todos,
-                {
-                    text: todoText,
-                    id: getId(),
-                    isCompleted: false,
-                },
-            ],
-        })),
+export const useTodoStore = create<TodoStore>()(
+    persist(
+        (set) => ({
+            todos: [],
+            isCompleted: false,
+            addTodo: (todoText: string) =>
+                set((state) => ({
+                    todos: [
+                        ...state.todos,
+                        {
+                            text: todoText,
+                            id: getId(),
+                            isCompleted: false,
+                        },
+                    ],
+                })),
 
-    deleteTodo: (todoId: number) =>
-        set((state) => ({
-            todos: state.todos.filter((todo: Todo) => todo.id !== todoId),
-        })),
+            deleteTodo: (todoId: number) =>
+                set((state) => ({
+                    todos: state.todos.filter((todo: Todo) => todo.id !== todoId),
+                })),
 
-    completeTodo: (todoId: number) =>
-        set((state) => ({
-            todos: state.todos.map((todo: Todo) =>
-                todo.id === todoId ? { ...todo, isCompleted: !todo.isCompleted } : todo
-            ),
-        })),
-}));
+            completeTodo: (todoId: number) =>
+                set((state) => ({
+                    todos: state.todos.map((todo: Todo) =>
+                        todo.id === todoId ? { ...todo, isCompleted: !todo.isCompleted } : todo
+                    ),
+                })),
+        }), // 그냥 하면 로컬스토리지에 저장인데 storage에 옵션 추가해주어서 세션스토리지에도 저장 가능
+        { name: 'todo_storage', storage: createJSONStorage(() => sessionStorage) }
+    )
+);
